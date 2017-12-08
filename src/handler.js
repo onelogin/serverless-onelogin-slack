63f156a8beb11c01dfd9a25c13aa661b75048a29
Rich Chetwynd <richardchetwynd@gmail.com>
2017-12-08 14:13:59 +1300

    a nifty little webhook -> slack handler

---
+++ src/handler.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var http = require('https');
+var config = require('./config');
+
+function sendToSlack(event){
+
+  var timestamp = new Date(event.event_timestamp).getTime();
+
+  var message = JSON.stringify({
+    channel: "#" + process.env.SLACK_CHANNEL,
+    username: "onelogin",
+    response_type: "in_channel",
+    attachments: [{
+      color: "#ff0000",
+      text: event.user_name + " had a risky login to OneLogin",
+      ts: timestamp,
+      fields: [
+        {
+          title: "Risk Score",
+          value: event.risk_score + " / 100",
+          short: false
+        },
+        {
+          title: "Risk Reasons",
+          value: event.risk_reasons,
+          short: false,
+          mrkdwn: false
+        },
+        {
+          title: "Notes",
+          value: event.notes,
+          short: false,
+          mrkdwn: false
+        }
+      ]
+    }]
+  });
+
+  var options = {
+      host: 'hooks.slack.com',
+      port: '443',
+      path: process.env.SLACK_PATH,
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(message)
+      }
+  };
+
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+  });
+
+  req.write(message);
+  req.end();
+}
+
+module.exports.hook = (event, context, callback) => {
+
+  var events = JSON.parse(event.body);
+
+  for(var i=0; i < events.length; i++){
+    if(events[i].event_type_id == 5 && events[i].risk_score > process.env.RISK_THRESHOLD){
+      sendToSlack(events[i]);
+    }
+  }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Ok'
+    }),
+  };
+
+  callback(null, response);
+};
